8ef2c980dd4f3b6437ad72cc11ea5b5361de24b7
radicaljj <121260419+radicaljj@users.noreply.github.com>
2024-04-14 09:34:51 -0400

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int firstNumber = 32;
+    printf("firstNumber = %d", firstNumber);
+
+    int secondNumber = firstNumber;
+    printf("/n secondNumber =%d", secondNumber);
+
+    return 0;
+}
